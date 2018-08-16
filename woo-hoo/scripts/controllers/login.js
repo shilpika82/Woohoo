6b804ed72a9791d9c6b52a69caa1ceab856e85fc
shilpika82 <shilpika82@gmail.com>
2018-08-16 18:58:54 +0530

    aaa

---
+++ woo-hoo/scripts/controllers/login.js
@@ -0,0 +1,59 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yapp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of yapp
+ */
+angular.module('yapp')
+  .controller('LoginCtrl', function($scope, $location, $http, $rootScope) {
+
+    $scope.submit = function() {
+        localStorage.setItem("username", $scope.username);
+        var formData = {
+            "username": $scope.username,
+            "password": $scope.password
+        };
+
+        $http({
+            method: 'POST',
+            url: $rootScope.baseUrl + '/login',
+            headers: {
+                'Access-Control-Allow-Origin' : '*',
+                'Access-Control-Allow-Methods' : 'POST, GET, OPTIONS, PUT',
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            data: formData
+        }).then(
+            function (response) {
+                console.log(response.data)
+                $scope.loginData  = response.data;
+                if(response.data.login.status == "200"){
+                    $location.path('/dashboard');
+                }else{
+                    $scope.loginError = 1;
+                }
+            }, function (error) {
+                console.log(error)
+            });
+
+
+
+      return false;
+    }
+
+  $scope.close = function(){
+      $scope.loginError = 0;
+
+  }
+
+  $scope.signup = function() {
+
+      $location.path('/signup');
+
+  }
+
+  });
---
+++ woo-hoo/scripts/controllers/ratings.js
@@ -0,0 +1,63 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yapp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of yapp
+ */
+angular.module('yapp')
+    .controller('RatingsCtrl', function($scope, $http, $rootScope, $state, $modalInstance, itemDetails) {
+        $scope.$state = $state;
+        console.log(itemDetails)
+        $scope.itemData = itemDetails;
+        $scope.loggedInUserName =  localStorage.getItem("username");
+        $scope.close = function () {
+            $modalInstance.dismiss('cancel');
+        };
+
+        $scope.rating = 0;
+        $scope.ratings = [{
+            current: 1,
+            max: 5
+        }];
+
+        $scope.getSelectedRating = function (rating) {
+            console.log(rating);
+        }
+
+        $scope.sendRate = function(){
+            console.log($scope.ratings[0].current);
+            /*alert("Thanks for your rating:" + $scope.ratings[0].current)*/
+            //alert("Thanks for your rates!\n\nFirst Rate: " + $scope.ratings[0].current+"/"+$scope.ratings[0].max
+            //  +"\n"+"Second rate: "+ $scope.ratings[1].current+"/"+$scope.ratings[0].max)
+
+            var formData = {
+                "rating": $scope.ratings[0].current.toString(),
+                "comments": $scope.comments
+            };
+            console.log(formData);
+
+            $http({
+                method: 'POST',
+                url: $rootScope.baseUrl + '/feedback/' + $scope.loggedInUserName,
+                headers: {
+                    'Access-Control-Allow-Origin' : '*',
+                    'Access-Control-Allow-Methods' : 'POST, GET, OPTIONS, PUT',
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+                data: formData
+            }).then(
+                function (response) {
+                    console.log(response.data);
+                    $modalInstance.dismiss('cancel');
+
+                }, function (error) {
+                    console.log(error)
+                });
+            return false;
+
+        }
+    });
